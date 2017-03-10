263548a52ab9752f86873b8527e34404a99d2d12
masa69 <psychedelic.nekopunch@gmail.com>
2017-03-10 12:39:27 +0900

    preg

--- preg.php
+++ preg.php
@@ -20,4 +20,30 @@ var_dump(preg_match('/^[0-9]+$/i', $test2));
 
 $mail = 'sample-mail_addrees.yeah@i.softbank.jp';
 var_dump(preg_match('/^[0-9a-z-._]+@[0-9a-z-._]+[0-9a-z]+$/i', $mail));
+
+
+
+
+$links = [
+	'http://test-sample.com/ TEST',
+	'http://test.sample.co.jp/ls_rar/ TEST',
+	'https://youtu.be/sample TEST',
+	'http://www.sample.co.jp/te/s/t7720/ TEST',
+	'http://lyricalschool.com/ TEST',
+	'http://test-sample.com TEST',
+	'http://test.sample.co.jp/ls_rar ',
+	'https://youtu.be/sample ',
+	'http://www.sample.co.jp/te/s/t7720 ',
+	'http://sample.com',
+	'http://日本語.com',
+	'http://日本語.com?test=sample&ja=日本',
+];
+
+// $ptn = "/(http|https):\/\/[a-z]+[a-z.-]*[a-z]+\.[a-z]+[a-z0-9.-_\/]*(\s|\n|[a-z0-9\/]+$)/i";
+$ptn = "/(http|https):\/\/[\S]+(\s|\n|[\S]+$)/i";
+
+foreach ($links as $link) {
+	echo (preg_match($ptn, $link, $linky)) ? "$linky[0]\n" : "ERROR: {$link}\n";
+}
+
 ?>
