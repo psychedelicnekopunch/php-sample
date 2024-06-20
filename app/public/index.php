901acf71996b0d435e172277b59bc743d789544b
masa <psychedelic.nekopunch@gmail.com>
2024-06-20 14:49:15 +0900

    add

---
+++ app/public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>docker-sample/nginx-php-fpm</title>
+	</head>
+	<body>
+		<h1>hello world</h1>
+		<p>docker-sample/nginx-php-fpm</p>
+		<p><a href="index.html">index.html &gt;&gt;</a></p>
+		<hr>
+		<?php phpinfo(); ?>
+	</body>
+</html>
